02ed4703414882bbaea7c4acd8d658573053880c
Anshikapal05 <anshikapal546@gmail.com>
2025-10-05 13:13:44 +0530

    added in committee page

--- src/pages/committees/AkbarIbadatKhanaPage.jsx
+++ src/pages/committees/AkbarIbadatKhanaPage.jsx
@@ -55,19 +55,13 @@ function AkbarIbadatKhanaPage() {
               Executive Board Members
             </a>
             <a
-              href="/coming-soon"
-              className="inline-block border border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-black font-semibold px-4 py-2 rounded-md transition-colors"
-            >
-              Background Guide
-            </a>
-            {/* <a
-              href="https://example.com/background-guide"
+              href="https://drive.google.com/file/d/1una9MrdckcjQaQ5kCzcyHiluwsuVJI-u/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-block border border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-black font-semibold px-4 py-2 rounded-md transition-colors"
             >
               Background Guide
-            </a> */}
+            </a>
           </div>
         </div>
 
